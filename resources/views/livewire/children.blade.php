14a1558f8356c3cf3b9deedfe7ec539848494387
Gabriel Ravelo <ravelogabriel2@gmail.com>
2024-03-04 00:51:29 -0400

    cap 17

---
+++ resources/views/livewire/children.blade.php
@@ -0,0 +1,5 @@
+<div>
+    <p>Hola desde el componente hijo</p>
+
+    <p>{{ $name }}</p>
+</div>
---
+++ resources/views/livewire/father.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1 class="text-2xl font-semibold">Soy el componente padre</h1>
+
+    <div>
+        <livewire:children :$name />
+    </div>
+</div>
